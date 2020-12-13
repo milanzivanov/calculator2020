0c2fc9140fbe3c30540d138100393b0d8e138c71
milan.zivanov <milan.zivanov@gmail.com>
2020-12-13 15:09:52 +0100

    2 js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
-const keys = document.querySelector('.calculator__keys');
-const display = document.querySelector('.calculator__display');
+const calculator = document.querySelector('.calculator');
+const keys = calculator.querySelector('.calculator__keys');
+const display = calculator.querySelector('.calculator__display');
 
 keys.addEventListener('click', e => {
     if(!e.target.closest('button')) return;
@@ -7,6 +8,21 @@ keys.addEventListener('click', e => {
     const key = e.target;
     const keyValue = key.textContent;
     const displayValue = display.textContent;
+    const type = key.dataset.type;
 
-    display.textContent = keyValue;
+    // is this a umber key?
+    if(type === 'number') {
+        if(displayValue === '0') {
+            display.textContent = keyValue;
+        } else {
+            display.textContent = displayValue + keyValue;
+        }
+    }
+
+    // is this a opereter key?
+    if(type === 'operator') {
+        console.log(key);
+
+        calculator.dataset.previousKeyType = 'operator';
+    }
 });
