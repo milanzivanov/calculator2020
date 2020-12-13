3feb54c54dd224b60bda0d68e9e799c4ba97aa25
milan.zivanov <milan.zivanov@gmail.com>
2020-12-13 16:53:36 +0100

    3 js

--- js/main.js
+++ js/main.js
@@ -8,21 +8,44 @@ keys.addEventListener('click', e => {
     const key = e.target;
     const keyValue = key.textContent;
     const displayValue = display.textContent;
-    const type = key.dataset.type;
+    const { type } = key.dataset;
+    const { previousKeyType } = calculator.dataset
 
-    // is this a umber key?
+    // is this a number key?
     if(type === 'number') {
+
         if(displayValue === '0') {
             display.textContent = keyValue;
-        } else {
+        } 
+        
+        else if (previousKeyType === 'operator') {
+            display.textContent = keyValue;
+        } 
+        
+        else {
             display.textContent = displayValue + keyValue;
         }
+
     }
 
     // is this a opereter key?
-    if(type === 'operator') {
-        console.log(key);
-
-        calculator.dataset.previousKeyType = 'operator';
-    }
+    if (type === 'operator') {
+        const operatorKeys = keys.querySelectorAll('[data-type="operator"]');
+        operatorKeys.forEach(el => { el.dataset.state = '' });
+        key.dataset.state = 'selected';
+    
+        calculator.dataset.firstNumber = displayValue;
+        calculator.dataset.operator = key.dataset.key;
+      }
+    
+      if (type === 'equal') {
+        // Perform a calculation
+        const firstNumber = calculator.dataset.firstNumber;
+        const operator = calculator.dataset.operator;
+        const secondNumber = displayValue;
+        // display.textContent = calculate(firstNumber, operator, secondNumber);
+        console.log(firstNumber, operator, secondNumber);
+      }
+    
+      calculator.dataset.previousKeyType = type;
 });
